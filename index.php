1d1e0bdd49de5ce43907ccea0e35fb925fcdf70c
oscarHDC <oscarhdec01@gmail.com>
2023-02-25 20:47:15 +0100

    UpdatePixel funcionality

--- index.php
+++ index.php
@@ -6,6 +6,20 @@ $client = new SoapClient(NULL, $options);
 //Obtenemos pixels
 $pixels = $client->getPixels();
 
+//$client->updateColor(1, 2, "#262222");
+
+
+
+
+
+if(isset($_POST["color"])){
+    $client->updateColor($_POST["y"], $_POST["x"], $_POST["color"]);
+}
+
+
+/* echo "tu usuario es: ".$usuario; 
+echo "contraseña es: ".$contra; */
+
 
 
 
@@ -17,14 +31,14 @@ $pixels = $client->getPixels();
 <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <title>Ejemplo Tema 7: Validación formulario con jQuery</title>
-        <script type="text/javascript"
-        src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <script type="text/javascript" src="jquery.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
     </head>
 <body>
     <h1>TABLA PIXELS</h1>
     <div id="tablaCont"></div>
    <p id="test"></p>
+   <p id="resultado"></p>
+   <input type="button" id="btn" value="Ingresar" />
 </body>
 <script type="text/javascript">
     var jArray =<?php echo json_encode($pixels); ?>;
@@ -34,17 +48,3 @@ $pixels = $client->getPixels();
 <script type="text/javascript" src="jquery.js"></script>
 </html>
 
-
-
-<?php 
-/* $pixels = $client->getPixels();
-for ($col=0; $col < 1 ; $col++) { 
-    echo "<tr>";
-    for($row = 0; $row < 1; $row++){
-        $eyedroper = "colorpick-eyedropper-active='false'";
-        $pixel = $pixels[$col*32];
-        echo "<td><input  type='color' value='{$pixel["color"]}' name='p' id='p' {$eyedroper}></td>";
-    }
-    echo "</tr>";
-}; */
-?>     
--- jquery.js
+++ jquery.js
@@ -1,29 +1,63 @@
 function crearTabla(json) {
-    let perchaTabla=document.createDocumentFragment();
-    let tabla=document.createElement("table");
-    let columna=0;
-    let tr;
-    let arrayPos=0;
-    for (let c = 0; c< 32; c++) {
-        tr=document.createElement("tr");
-        for (let f = 0; f < 32; f++) {
-            let color=json[arrayPos][2]
-            let td=document.createElement("td");
-            let inputColor=document.createElement("input")
-            inputColor.setAttribute("type","color");
-            inputColor.value=color;
-            td.append(inputColor);
-            tr.append(td);
-            tabla.append(tr);
-            arrayPos+=32;
-        }
-        arrayPos+=(-32*32);
-        arrayPos++;
+  let perchaTabla = document.createDocumentFragment();
+  let tabla = document.createElement("table");
+  /* let columna = 0; */
+  let tr;
+  let arrayPos = 0;
+  for (let c = 0; c < 32; c++) {
+    tr = document.createElement("tr");
+    for (let f = 0; f < 32; f++) {
+      let color = json[arrayPos][2];
+      let td = document.createElement("td");
+      let inputColor = document.createElement("input");
+      inputColor.setAttribute("type", "color");
+      inputColor.value = color;
+      inputColor.name = `${c} ${f}`;
+
+      $(document).on("ready", function () {
+        $(inputColor).change(function () {
+          var url = "index.php";
+          $.ajax({
+            type: "POST",
+            url: url,
+            data: {
+              color: inputColor.value,
+              x: inputColor.name.split(" ")[0],
+              y: inputColor.name.split(" ")[1],
+            },
+
+            success: function (data) {
+              //console.log(inputColor.name.split(" ")[0]);
+            },
+          });
+        });
+      });
+
+      td.append(inputColor);
+      tr.append(td);
+      tabla.append(tr);
+      arrayPos += 32;
     }
-    perchaTabla.append(tabla);
-    let div=document.getElementById("tablaCont").append(perchaTabla);
+    arrayPos += -32 * 32;
+    arrayPos++;
+  }
+  perchaTabla.append(tabla);
+  let div = document.getElementById("tablaCont").append(perchaTabla);
 }
 
+/* $(document).on("ready", function () {
+  $("#btn").click(function () {
+    var url = "index.php";
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: { color: "ola", contrasena: "adios" },
+      success: function (data) {
+        //  $("#resultado").html("ola");
+      },
+    });
+  });
+}); */
 
 /* $(document).ready(function (e) {
     e.preventDefault();
@@ -51,7 +85,5 @@ function crearTabla(json) {
       });
  */
 
-
-
-
-      crearTabla(jArray);
+const interval = setInterval(crearTabla(jArray), 5000);
+//crearTabla(jArray);
